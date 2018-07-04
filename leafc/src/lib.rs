a1706272b887c8a4678c95625f157f3bec8f8bcf
Benny A <virusdownloader7@gmail.com>
2018-07-03 18:21:34 -0700

    Add tests (that fail)

--- leafc/src/lib.rs
+++ leafc/src/lib.rs
@@ -7,6 +7,8 @@ use std::path::Path;
 pub mod ast;
 pub mod codegen;
 pub mod failure;
+#[cfg(test)]
+mod tests;
 
 pub fn leafc<P: AsRef<Path>>(path: P) {
 	let _ast = ast::create_ast(path);
---
+++ leafc/src/tests/basic.rs
@@ -0,0 +1,28 @@
+use std::path::Path;
+
+use ast;
+use codegen;
+
+fn load_file<P: AsRef<Path>>(path: P) -> String {
+	unimplemented!()
+}
+
+#[test]
+fn basic_tests() {
+	let tests = [
+		"let a := 5;".to_owned(),
+		"let a := 7".to_owned(),
+		"let a := loop { break 3; };".to_owned(),
+		"let a := if 1 then loop break 15; back + 10 else { 200 + if 0 then 250 else loop back end } + 500 end; a".to_owned(),
+	];
+	
+	for test in tests.into_iter() {
+		let lexed = ast::lexer::lex(&test).unwrap();
+		let mut tokenizer = ast::tokenizer::Tokenizer::new(lexed);
+		let tokens = tokenizer.tokenize().unwrap();
+		let tokentree = ast::treeify::treeify(&tokens.tokens).unwrap();
+		let ast = ast::parser::parse(tokentree.as_slice()).unwrap();
+		let code_generator = codegen::vmgen::CodeGenerator::new();
+		let _instructions = code_generator.gen_instructions(ast);
+	}
+}
